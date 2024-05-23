6401c3596a80d93c39f31a464366995fc238d1b3
Naim2000 <naimarnohero@gmail.com>
2024-05-23 13:06:45 -0500

    wait on SD card

--- source/sd.c
+++ source/sd.c
@@ -16,8 +16,60 @@ bool SDMount(void) {
 	if (sdMounted) return true;
 
 	sdmc->startup();
-	return (sdMounted = (f_mount(&fs, "sdmc:/", 1) == FR_OK));
 
+	char spin[] = "|/-\\";
+	char* p = spin;
+	bool inserted = sdmc->isInserted();
+	while (!inserted) {
+		printf(pWarn "Please insert your SD card. [%c]\r", *p++);
+		scanpads();
+		if (buttons_down(WPAD_BUTTON_HOME)) break;
+
+		if (!*p) {
+			p = spin;
+			sdmc->startup();
+			inserted = sdmc->isInserted();
+		}
+
+		usleep(100000);
+	}
+	clearln();
+
+	if (!inserted) {
+		printf(pBad "No SD card inserted!\n");
+		return false;
+	}
+
+	printf(pInfo "Mounting sdmc:/ ...\r");
+	FRESULT fres = f_mount(&fs, "sdmc:/", true);
+	if (fres == FR_OK) {
+		unsigned long freeSpace = (fs.free_clst * fs.csize) * 0x200;
+		unsigned long totalSpace = ((fs.n_fatent - 2) * fs.csize) * 0x200;
+
+		puts(pGood "Mounting sdmc:/ OK!");
+		sdMounted = true;
+
+		if (totalSpace < 0x80000000) // 2GB
+		{
+			printf(pWarn "Your SD card is under 2GB? (%luMB)\n", totalSpace / 0x100000);
+			sleep(2);
+		}
+
+
+		if (freeSpace < 0x1000000) // 16MB
+		{
+			printf(pBad "Error #06: Insufficient SD card space!\n"
+				   pBad "At least 16MB is required, you have %.2fMB!", freeSpace / (float)0x100000);
+
+			SDUnmount();
+		}
+
+	}
+	else {
+		printf(pBad "Mounting sdmc:/ failed! (%i)\n", fres);
+	}
+
+	return sdMounted;
 }
 
 void SDUnmount(void) {
--- source/video.h
+++ source/video.h
@@ -5,7 +5,7 @@
 
 #define pBad	"[\x1b[31;1mXX\x1b[39m]	"
 #define pGood	"[\x1b[32;1mOK\x1b[39m]	"
-#define pWarn	"[\x1b[33;1m!?\x1b[39m]	"
+#define pWarn	"[\x1b[33;1m??\x1b[39m]	"
 #define pInfo	"[--]	"
 #define pNote	"[\x1b[34;1m**\x1b[39m]	"
 
