2331d1d31452249e31759d3e01c0a85d2b8926d2
Naim2000 <naimarnohero@gmail.com>
2024-05-25 16:06:54 -0500

    user friendly interface

--- source/main.c
+++ source/main.c
@@ -12,21 +12,79 @@
 __weak_symbol __printflike(1, 2)
 void OSReport(const char* fmt, ...) {}
 
-int main(void) {
-	puts(
-		"MSET9-Wii installer by thepikachugamer\n"
-		"MSET9 exploit by zoogie https://github.com/zoogie/MSET9\n" );
+static void PrintHeader(void) {
+	clear();
 
+
+	puts("MSET9-Wii installer by thepikachugamer\n"
+		 "MSET9 exploit by zoogie - https://github.com/zoogie/MSET9");
+
+	clearln('\315');
+}
+
+int main(void) {
 	initpads();
 	if (!SDMount()) goto exit;
 
-	MSET9Start(1);
+	if (!MSET9Start()) goto exit;
 
-	if (!MSET9SanityCheck()) goto exit;
+	int consoleVer = 0;
 
-	puts("Inject MSET9 now?");
-	wait_button(0);
-	if (buttons_down(WPAD_BUTTON_PLUS)) MSET9Injection();
+	PrintHeader();
+	puts("\n"
+		 "	-+========= What is your console model and version? =========+-\n"
+		 "	Console Model: Old 3DS has 2 shoulder buttons (L, R)\n"
+		 "	               New 3DS has 4 shoulder buttons (L, R, ZL, ZR)\n\n"
+
+		 "	Console Version: Displayed on the main page of System Settings.\n"
+	);
+	const char* const options[4] = {
+		"Old 3DS/Old 3DS XL/Old 2DS,    11.8 up to 11.17 (latest!)",
+		"New 3DS/New 3DS XL/New 2DS XL, 11.8 up to 11.17 (latest!)",
+		"Old 3DS/Old 3DS XL/Old 2DS,    11.4 up to 11.7",
+		"New 3DS/New 3DS Xl/New 2DS XL, 11.4 up to 11.7"};
+
+	const char* const shortNames[4] = {
+		"Old 3DS, 11.8 up to 11.17",
+		"New 3DS, 11.8 up to 11.17",
+		"Old 3DS, 11.4 up to 11.7",
+		"New 3DS, 11.4 up to 11.7"
+	};
+
+	int posX, posY;
+	CON_GetPosition(&posX, &posY);
+
+	while (true) {
+
+		printf("\x1b[%i;0H", posY);
+		for (int ii = 0; ii < 4; ii++)
+			printf("	%s %s\x1b[40m\x1b[39m\n", ii == consoleVer ? "\x1b[47;1m\x1b[30m>>" : "  ", options[ii]);
+
+		wait_button(0);
+
+		if (buttons_down(WPAD_BUTTON_UP)) { if (!consoleVer--) consoleVer = 3; continue; }
+
+		else if (buttons_down(WPAD_BUTTON_DOWN)) { if (++consoleVer == 4) consoleVer = 0; continue; }
+
+		else if (buttons_down(WPAD_BUTTON_A)) { consoleVer++; break; }
+
+		else if (buttons_down(WPAD_BUTTON_B | WPAD_BUTTON_HOME)) { consoleVer = 0; break; }
+	}
+
+	if (!consoleVer) goto exit;
+
+	MSET9SetConsoleVer((MSET9Version)consoleVer);
+
+	PrintHeader();
+	if (MSET9SanityCheck()) {
+		printf(pNote "Selected console version: %s\n", shortNames[consoleVer - 1]);
+		printf(pNote "Inject MSET9 now?\n\n"
+
+			   pInfo "Press + to confirm.\n"
+			   pInfo "Press any other button to cancel.\n");
+		wait_button(0);
+		if (buttons_down(WPAD_BUTTON_PLUS)) MSET9Injection();
+	}
 
 exit:
 	SDUnmount();
